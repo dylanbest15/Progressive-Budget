d4c1c8a22a1f69833aad68e32fa90a33bf6bc727
Dylan Best <best.dylan15@gmail.com>
2020-10-25 14:04:05 -0400

    deploying

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static("public"));
 
 // connect to database
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  process.env.MONGODB_URI || 'mongodb://localhost/progressive-budget',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,

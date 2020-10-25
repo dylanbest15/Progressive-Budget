6140e249e5d262fff5a60214efc9ed0fb2f29d90
Dylan Best <best.dylan15@gmail.com>
2020-10-25 12:04:12 -0400

    connected to mongodb with dotenv

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+// requirements
+require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+// host on port
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,10 +18,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// connect to database
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
